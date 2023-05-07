f29b74f96fef563d8f1c062b54da71ba03175ee4
Hung Tran <hungtran891989@gmail.com>
2023-05-07 07:34:57 +0700

    feat: vendor input

--- src/components/StepOne.vue
+++ src/components/StepOne.vue
@@ -1,3 +1,30 @@
+<script setup lang="ts">
+const vendors = [{
+  id: '1',
+  description: 'Hewlett Packard Enterprise (HPE)',
+},
+{
+  id: '2',
+  description: 'Advanced Micro Devices (AMD)',
+},
+{
+  id: '3',
+  description: 'Sillicon Integrated Systems (SiS)',
+},
+{
+  id: '4',
+  description: 'Intel Corporation (Intel)',
+},
+{
+  id: '5',
+  description: 'Nvidia Corporation (Nvidia)',
+},
+{
+  id: '6',
+  description: 'Gigabyte Technology (GIGABYTE)',
+}]
+</script>
+
 <template>
   <h3 class="step-title text-center">
     <strong>Step 1: Enter Your Data</strong>
@@ -18,9 +45,17 @@
         >
           <b-form-input
             id="current-vendor"
+            list="current-vendors"
             type="text"
+            autocomplete="off"
             required
           />
+
+          <datalist id="current-vendors">
+            <option v-for="vendor in vendors" :key="vendor.id">
+              {{ vendor.description }}
+            </option>
+          </datalist>
         </b-form-group>
       </b-col>
     </b-form-row>
